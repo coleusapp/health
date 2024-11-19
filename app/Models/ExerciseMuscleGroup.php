0da56eb820d275d97e0ce1500ce3b95056193635
Amirmasoud <amirmasood33@gmail.com>
2024-11-18 18:36:34 -0700

    wip

--- app/Models/ExerciseMuscleGroup.php
+++ app/Models/ExerciseMuscleGroup.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class exerciseMuscleGroup extends Model
+class ExerciseMuscleGroup extends Model
 {
     public $incrementing = true;
 
