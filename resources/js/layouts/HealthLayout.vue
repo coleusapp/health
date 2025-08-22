d81944d7f89cec4e3b62308f895e772ccc35aac2
Amirmasoud <amirmasood33@gmail.com>
2025-08-21 21:38:45 -0600

    wip

--- resources/js/layouts/HealthLayout.vue
+++ resources/js/layouts/HealthLayout.vue
@@ -9,7 +9,7 @@ import Aside from '@coleus/health/components/Aside.vue';
             <div class="min-w-60 py-2 pl-2 pr-1">
                 <Aside />
             </div>
-            <div class="w-full h-[calc(100vh-1rem)] bg-white m-2 rounded-lg border border-gray-200 py-8 px-4">
+            <div class="w-full h-[calc(100vh-1rem)] bg-white dark:bg-gray-950 m-2 rounded-lg border border-gray-200 dark:border-gray-700 py-8 px-4">
                 <div class="w-full max-w-7xl mx-auto">
                     <slot />
                 </div>
