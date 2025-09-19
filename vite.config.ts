a1fb02dba290e91943201be12ac0dc2dc3d83f6e
Amirmasoud <amirmasood33@gmail.com>
2025-09-18 19:40:11 -0600

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig, mergeConfig } from 'vite';
 import baseConfig from '../support/base.config.js';
 import path from 'path';
+import laravel from 'laravel-vite-plugin';
 
 export default defineConfig(
     mergeConfig(baseConfig, {
         base: '/vendor/health/',
+        plugins: [
+            laravel({
+                input: ['resources/js/app.ts'],
+                refresh: true,
+                hotFile: path.resolve(__dirname, '../../public/hot'),
+            }),
+        ],
         resolve: {
             alias: {
                 '@': path.resolve(__dirname, 'resources/js'),
