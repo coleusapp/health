ce91a0928aae57546259338ce0533c3f9a6c19e6
Amirmasoud <amirmasood33@gmail.com>
2025-12-01 19:07:20 -0700

    wip

--- resources/js/components/settings/GeneralCard.vue
+++ resources/js/components/settings/GeneralCard.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import General from '@/components/settings/General.vue';
+import Header from '@coleus/support/components/card/Header.vue';
 </script>
 
 <template>
     <UiCard>
+        <template #header>
+            <Header title="General Settings" />
+        </template>
         <General />
     </UiCard>
 </template>
